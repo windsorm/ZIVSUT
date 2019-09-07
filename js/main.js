c4b82346ac3152182502b8a18eaa027373011e00
Kateřina Broučková <brouckova@Ondrej-iMac.hub20>
2019-09-07 14:08:27 +0200

    Submitting querries

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ $( document ).ready(function() {
     console.log( "ready!" );
     // alert("ready")
 
+    function onSubmit() {
+        console.log("bbbbbb");
+        var s = $("#search").val()
+        $(".inner-form").fadeOut()
+        $(".info").fadeOut()
+        $(".notchatbot").append("<div>" + s + "</div>")
+    }
 
-    $(".btn-search").on( "click", function() {
-        console.log( $( this ).text() );
-        alert("hledame: " + $("#search").val())
-    });
-
+    $(".btn-search").on( "click", onSubmit);
+    $( "#search" ).keypress(function( event ) {
+        console.log("aaabbb")
+        if ( event.which == 13 ) {
+            onSubmit()
+        }
+    })
 
 });
