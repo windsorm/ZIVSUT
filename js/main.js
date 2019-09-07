d25cc0071989f8cd9b329727725aa24da742add5
Vaclav Loffelmann <loffelmann.vaclav@seznam.cz>
2019-09-07 11:51:06 +0200

    add dialogue

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    // alert("ready")
 
 
+    $(".btn-search").on( "click", function() {
+        console.log( $( this ).text() );
+        alert("hledame: " + $("#search").val())
+    });
+
+
+});
